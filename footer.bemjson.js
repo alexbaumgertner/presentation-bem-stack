ddd53731e35b3c53d9f6ad27d644d094932ddcc6
alexbaumgertner <alex.baumgertner@gmail.com>
2015-11-29 20:45:14 +0300

    Примеры footer

---
+++ footer.bemjson.js
@@ -0,0 +1,31 @@
+({
+    block: 'footer',
+    content: [
+        {
+            elem: 'col',
+            mods: [{ position: 'left' }],
+            content: [
+                { block: 'logo' },
+                { block: 'social', providers: ['vk', 'facebook', 'twitter'] },
+                { block: 'copyright' }
+            ]
+        },
+        {
+            elem: 'col',
+            mods: [{ position: 'right' }],
+            content: [
+                {
+                    block: 'menu',
+                    mods: [{section: 'learning'}],
+                    content: [
+                        { elem: 'item', url: '/learn_map', title: 'Карта обучения' },
+                        { elem: 'item', url: '/courses',   title: 'Онлайн-курсы' },
+                        { elem: 'item', url: '/intensive', title: 'Интенсив' },
+                        { elem: 'item', url: '/pricing',   title: 'Подписка' },
+                        { elem: 'item', url: 'Рейтинг',    title: '/rating' }
+                    ]
+                }
+            ]
+        }
+    ]
+});
