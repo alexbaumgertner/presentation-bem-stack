8632d6957f849569e7547ff2ebd0341688bf17c5
alexbaumgertner <alex.baumgertner@gmail.com>
2016-01-28 00:11:01 +0300

    Refactor ace editors with promises

--- js/ace-sample-frame.js
+++ js/ace-sample-frame.js
@@ -1,86 +1,127 @@
+// Ace editor loader
 (function(global) {
-    var aceSrc = 'https://cdn.jsdelivr.net/g/ace@1.2.3(' +
-        'noconflict/ace.js+' +
-        'noconflict/mode-html.js+' +
-        'noconflict/theme-chrome.js+' +
-        'noconflict/mode-json.js)';
+  var aceSrc = 'https://cdn.jsdelivr.net/g/ace@1.2.3(' +
+    'noconflict/ace.js+' +
+    'noconflict/mode-html.js+' +
+    'noconflict/theme-chrome.js+' +
+    'noconflict/mode-json.js)';
 
-    loader(global.document, aceSrc, run);
+  var aceEditorsLoader = new Promise(function(resolve, reject) {
+    loader(global.document, aceSrc)
+      .then(aceEditorsInit)
+      .then(function(aceEditorsPromises) {
+        Promise
+          .all(aceEditorsPromises)
+          .then(function(result) {
+            resolve(result);
+          });
+      });
+  });
 
-    function run() {
-        var
-            sampleNodes = global.document.querySelectorAll('.ace-sample-frame'),
-            forEach = global.Array.prototype.forEach;
+  /**
+   * Init Ace editors on DOM nodes
+   *
+   * @returns {Array} Promises
+   */
+  function aceEditorsInit() {
 
-        // TODO: try http://prismjs.com/plugins/line-numbers/
+    var sampleNodes = global.document.querySelectorAll('.ace-sample-frame'),
+      samplePromises = [],
+      forEach = global.Array.prototype.forEach;
 
-        forEach.call(sampleNodes, function (sampleNode) {
-            var
-                src = sampleNode.dataset.src,
-                lang = sampleNode.dataset.lang,
-                fileType = src.split('.').pop(),
-                editor = ace.edit(sampleNode);
+    // TODO: try http://prismjs.com/plugins/line-numbers/
 
-            // fix deprecated
-            editor.$blockScrolling = Infinity;
+    forEach.call(sampleNodes, function(sampleNode) {
+      var promiseResolver;
 
-            if (fileType === 'js') {
-                fileType = 'javascript';
-            }
+      var promise = new Promise(function(resolve, reject) {
+        promiseResolver = resolve;
+      });
 
-            fetch(sampleNode.dataset.src)
-                .then(function (response) {
-                    return response.text()
-                })
-                .then(function (code) {
+      samplePromises.push(promise);
 
-                    if (lang === 'bemhtml') {
+      var
+        src = sampleNode.dataset.src,
+        lang = sampleNode.dataset.lang,
+        fileType = src.split('.').pop(),
+        editor = ace.edit(sampleNode);
 
-                        editor.getSession().setValue(code);
+      // fix deprecated
+      editor.$blockScrolling = Infinity;
 
-                    } else {
-                        editor.getSession().setValue(code);
-                    }
+      if (fileType === 'js') {
+        fileType = 'javascript';
+      }
 
-                    editor.getSession().setMode('ace/mode/' + fileType);
-                })
+      fetch(sampleNode.dataset.src)
+        .then(function(response) {
+          return response.text()
+        })
+        .then(function(code) {
+          editor.getSession().setValue(code);
+          editor.getSession().setMode('ace/mode/' + fileType);
+
+          sampleNode.aceEditor = editor;
+          promiseResolver({ editor: editor, code: code });
         });
-    }
+    });
 
+    return samplePromises;
+  }
 
-    /**
-     * Load javascript
-     *
-     * @param {Object} document
-     * @param {String} path
-     * @param {Function} callback
-     */
-    function loader(document, path, callback) {
-        var head = document.getElementsByTagName('head')[0];
-        var script = document.createElement('script');
+  /**
+   * Load javascript
+   *
+   * @param {Object} document
+   * @param {String} path
+   * @return {Object} Promise
+   */
+  function loader(document, path) {
+    var head = document.getElementsByTagName('head')[0];
+    var script = document.createElement('script');
+    var callback;
 
-        script.type = 'text/javascript';
-        script.charset = 'utf-8';
-        script.src = (location.protocol === 'file:' && !path.indexOf('//') ? 'http:' : '') + path;
+    var promise = new Promise(function(resolve, reject) {
+      callback = resolve;
+    });
 
-        if ('onload' in script) {
-            script.onload = script.onerror = function () {
-                script.onload = script.onerror = null;
-                callback();
-            };
+    script.type = 'text/javascript';
+    script.charset = 'utf-8';
+    script.src = (location.protocol === 'file:' && !path.indexOf('//') ? 'http:' : '') + path;
 
-        } else {
-            script.onreadystatechange = function () {
-                var readyState = this.readyState;
+    if ('onload' in script) {
+      script.onload = script.onerror = function() {
+        script.onload = script.onerror = null;
+        callback();
+      };
 
-                if (readyState === 'loaded' || readyState === 'complete') {
-                    script.onreadystatechange = null;
-                    callback();
-                }
-            };
+    } else {
+      script.onreadystatechange = function() {
+        var readyState = this.readyState;
+
+        if (readyState === 'loaded' || readyState === 'complete') {
+          script.onreadystatechange = null;
+          callback();
         }
-
-        head.appendChild(script);
+      };
     }
+
+    head.appendChild(script);
+
+    return promise;
+  }
+
+
+  aceEditorsLoader
+    .then(function(result) {
+      var doc = global.document;
+
+      var BEMHTML = global['BEMHTML_BARE'];
+      var bemjsonNode = doc.getElementById('logo-example-bemjson');
+      var bemhtmlNode = doc.getElementById('logo-bemhtml');
+      var htmlNode = doc.getElementById('logo-sample-html');
+
+      console.log("bemjsonNode.aceInit: ", bemjsonNode.aceEditor);
+    });
 
 }(window));
