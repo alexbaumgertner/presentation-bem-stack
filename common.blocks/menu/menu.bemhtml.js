5cc6d11b44cb96c4ee22d818fcb09ab63b1de786
alexbaumgertner <alex.baumgertner@gmail.com>
2016-02-01 10:45:54 +0300

    Поправить пример menu

--- common.blocks/menu/menu.bemhtml.js
+++ common.blocks/menu/menu.bemhtml.js
@@ -1,20 +1,15 @@
-// match `menu` block
+// match `menu` block in BEMJSON
 block('menu')(
     // match `list` element of `menu` block (`menu__list`)
-    elem('list')(
-        tag()('ul')
-    ),
+    elem('list')( tag()('ul') ),
 
     // match `item` element of `menu` block (`menu__item`)
-    elem('item')(
-        tag()('li'),
-        content()(function() {
+    elem('item')( tag()('li'), content()(function() {
             return {
                 block: 'link',
                 tag: 'a',
                 attrs: { href: this.ctx.url },
                 content: this.ctx.title
             };
-        })
-    )
+        }) )
 );
--- common.blocks/menu/menu.bemtree.js
+++ common.blocks/menu/menu.bemtree.js
@@ -1,27 +1,15 @@
 // match `menu` block
-block('menu')(
-    content()(function() {
-
-        var ctx = this.ctx,
-            content = ({
-                block: 'menu',
-                mods: { section: 'learning' },
-                content: [
-                    { elem: 'title', content: ctx.title },
-                    {
-                        elem: 'list',
-                        content: ctx.items.map(
-                            function(item) {
-                                return {
-                                    elem: 'item',
-                                    url: item.url,
-                                    title: item.title
-                                };
-                            })
-                    }
-                ]
-            });
-
-        return content;
-    })
+block('menu')( // this.ctx
+  content()(function() {
+    return ({
+      block: 'menu',
+      mods: { section: 'learning' },
+      content: [
+        { elem: 'title', content: this.ctx.title },
+        { elem: 'list',  content: this.ctx.items.map(
+          function(item) { return { elem: 'item', url: item.url, title: item.title }; })
+        }
+      ]
+    });
+  })
 );
